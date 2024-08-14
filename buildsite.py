25dab5fb4e748c0646b3c799ab51112e7003f573
jatinsoni006 <32970092+jsonirobots@users.noreply.github.com>
2024-08-13 20:59:25 -0400

    check

--- buildsite.py
+++ buildsite.py
@@ -3,19 +3,14 @@ import sys
 
 cmds = ["poetry run jupyter-book build portfolio",
         "git add -A",
-        "git commit -m ",
+        "git commit -m",
         "git push",
         "poetry run ghp-import -n -p -f portfolio/_build/html"]
-cmds = [
-        "git add -A",
-        "git commit -m ",
-        "git push"]
-defmsg = '"updated portfolio site"'
+defmsg = 'updated portfolio site'
 
 for cmd in cmds:
     if "commit" in cmd:
         if len(sys.argv) > 1:
             defmsg = sys.argv[1]
-        cmd = cmd+defmsg
-    print(cmd)
+        cmd = f'{cmd} "{defmsg}"'
     os.system(cmd)
